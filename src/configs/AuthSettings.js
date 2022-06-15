da3c01c21790caafb6fa1729662c025e79663d33
CiocanGeorge <george.ciocan@crmsoftware.ro>
2022-06-15 14:14:58 +0300

    configs and add react-icons

---
+++ src/configs/AuthSettings.js
@@ -0,0 +1,9 @@
+const authSettings = {
+    rolesKey: "http://Moby-BookLibrary-v1/roles",
+    domain: "dev-ikjp029m.eu.auth0.com",
+    clientId: "RhPk3ZejLfOOB7QcyBID557OOZaUHmq0",
+    audience: "Moby-BookLibrary-v1"
+  };
+  
+  export { authSettings };
+  
